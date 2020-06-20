78ed7718936bc78afa84dd8c326ab5d0566d975e
renato.soares <renatoferreira656@gmail.com>
2020-06-20 17:17:50 -0300

    feature: file

--- src/main/java/com/test/directions/helper/FilesHelper.java
+++ src/main/java/com/test/directions/helper/FilesHelper.java
@@ -7,6 +7,7 @@ import org.springframework.web.multipart.MultipartFile;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -18,7 +19,7 @@ public class FilesHelper {
         if(file.isEmpty()){
             return null;
         }
-        try (BufferedReader reader = new BufferedReader(new InputStreamReader(file.getInputStream()))) {
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(file.getInputStream(), StandardCharsets.UTF_8))) {
             Set<String> lines = new HashSet<>();
             String line = reader.readLine();
             while(line != null){
