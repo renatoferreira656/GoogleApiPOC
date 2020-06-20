de08e18699f4f4d4125bc78072b53a74caace9fb
renato.soares <renatoferreira656@gmail.com>
2020-06-20 10:13:16 -0300

    test: simple test

---
+++ src/test/java/com/test/directions/controller/HealthCheckControllerTest.java
@@ -0,0 +1,15 @@
+package com.test.directions.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class HealthCheckControllerTest {
+
+    @Test
+    public void testHealthCheck(){
+        String result = new HealthCheckController().healthCheck();
+        assertEquals(result, "{\"working\": \"true\" }");
+
+    }
+}
