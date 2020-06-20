8de8a7f3b9b86ea18c033dd9cd66f2aae36c601e
renato.soares <renatoferreira656@gmail.com>
2020-06-20 15:06:35 -0300

    setting application as properites

--- src/main/java/com/test/directions/service/DirectionsService.java
+++ src/main/java/com/test/directions/service/DirectionsService.java
@@ -4,6 +4,7 @@ import com.test.directions.json.Json;
 import com.test.directions.model.geocode.GeoCodeResults;
 import com.test.directions.model.geocode.GeoPoint;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import java.util.HashMap;
@@ -12,16 +13,20 @@ import java.util.Map;
 @Component
 public class DirectionsService {
 
+    @Value("${spring.google.api.key}")
+    private String googleApiKey;
+
     @Autowired
     private HTTPService httpService;
 
-
     public GeoPoint findCoordinatesBy(String location) {
-        Map<String, String> query = new HashMap<String, String>();
+        Map<String, String> query = new HashMap<>();
         query.put("address", location);
-        query.put("key", "_");
+        query.put("key", googleApiKey);
         GeoCodeResults json = httpService.get(GeoCodeResults.class, "https://maps.googleapis.com/maps/api/geocode/json", query);
-        System.out.println(Json.toJson(json));
+        if(json.getResults().isEmpty()){
+            return null;
+        }
         return json.getResults().get(0).getGeometry().getLocation();
     }
 
