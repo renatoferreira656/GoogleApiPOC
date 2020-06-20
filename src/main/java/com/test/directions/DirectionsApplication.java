02b91c370e4759aad09f7cf9fad849b38b7a9d2c
renato.soares <renatoferreira656@gmail.com>
2020-06-20 08:44:43 -0300

    feature: initial project

---
+++ src/main/java/com/test/directions/DirectionsApplication.java
@@ -0,0 +1,13 @@
+package com.test.directions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DirectionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DirectionsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/test/directions/DirectionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.directions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DirectionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
